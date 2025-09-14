3b60454b1c069685e8da36b1565c42baeb6e7386
Luciano <tubuendormir@gmail.com>
2025-09-14 16:50:49 -0300

    Cambio final

--- src/components/NavBarContainer.jsx
+++ src/components/NavBarContainer.jsx
@@ -1,6 +1,6 @@
 
 import { useEffect, useState } from 'react'
-import NavBar from 'NavBar'
+import NavBar from './NavBar'
 
 function NavBarContainer() {
     const [categories, setCategories] = useState([])
