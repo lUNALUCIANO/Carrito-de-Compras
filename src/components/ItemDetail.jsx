04d66ed7b92832bd94d04a9f07bd369ca0a19743
Luciano <tubuendormir@gmail.com>
2025-09-14 14:37:57 -0300

    Cambio de Componente

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -2,7 +2,7 @@ import Col from 'react-bootstrap/Col'
 import Container from 'react-bootstrap/Container'
 import Image from 'react-bootstrap/Image'
 import Row from 'react-bootstrap/Row'
-import Counter from './counter'
+import Counter from './Counter'
 
 function ItemDetail({item}) {
     return (
